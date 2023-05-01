92f57e306e7f9ca632c92196feb279ec10193a9a
anilboppuri <anil4java2@gmail.com>
2023-05-01 16:32:40 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+
+ public static void main(String args[]){
+ 
+         for(int i=1;i<=5;i++){
+		   System.out.println("It's never too late to start a good thing");
+		 }
+ }
+}
+
+//$ git remote add origin https://github.com/anilboppuri/devops.git
+//$ git push -u origin master
